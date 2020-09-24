44eb78662549d0197d79dc35bc0b81c62efdfc95
Eric Malley <EMalley998@gmail.com>
2020-09-24 18:02:54 -0400

    working post req

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //connect to DB
-mongoose.connect('mongodb://localhost/Artilces', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/Artilces', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch((err) => console.log(err))
 var db = mongoose.connection;
@@ -12,12 +13,15 @@ var db = mongoose.connection;
 var app = express();
 var PORT = 3000
 
-
-
 //load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//middleware for body parser//  
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+////////////
 
 ///////////////////////////////////////
 //check connection
@@ -50,9 +54,26 @@ app.get('/', function (req, res) {
 //add routes
 app.get('/articles/add', function (req, res) {
     res.render('addArticle', {
-        title: 'add'
+        title: 'Add Articles'
     })
 })
+
+//add Submit POST ROUTE
+app.post('/articles/add', function (req, res) {
+    var article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/')
+        }
+    })
+});
 
 //start server
 app.listen(PORT, function () {
